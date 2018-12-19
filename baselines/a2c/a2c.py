1d3425f8369441de4b26dc9749f08c2c4153cd09
Zafar Mahmood <zfar.mah@gmail.com>
2018-12-19 22:32:32 +0100

    a2c pong with curiosity

--- baselines/a2c/a2c.py
+++ baselines/a2c/a2c.py
@@ -287,9 +287,9 @@ def learn(
             logger.record_tabular("policy_entropy", float(policy_entropy))
             logger.record_tabular("value_loss", float(value_loss))
             if curiosity == True :
-                logger.record_tabular("forwardLoss", float(forwardLoss))
-                logger.record_tabular("inverseLoss", float(inverseLoss))
-                logger.record_tabular("icm Loss", float(icm_loss))
+                # logger.record_tabular("forwardLoss", float(forwardLoss))
+                # logger.record_tabular("inverseLoss", float(inverseLoss))
+                # logger.record_tabular("icm Loss", float(icm_loss))
 
             logger.record_tabular("explained_variance", float(ev))
             logger.dump_tabular()
